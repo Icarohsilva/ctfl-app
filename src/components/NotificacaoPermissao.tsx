05586dfcec5ba2f1f1c66e447fd42078afe94369
Icaro Silva <contatoicarosilva@outlook.com>
2026-04-29 01:04:30 -0300

    fix tipo applicationServerKey

--- src/components/NotificacaoPermissao.tsx
+++ src/components/NotificacaoPermissao.tsx
@@ -50,7 +50,7 @@ export default function NotificacaoPermissao() {
     const sw = await navigator.serviceWorker.ready;
     const subscription = await sw.pushManager.subscribe({
       userVisibleOnly: true,
-      applicationServerKey: urlBase64ToUint8Array(process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!) as unknown as string
+      applicationServerKey: urlBase64ToUint8Array(process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!) as unknown as BufferSource,
     });
 
     await fetch("/api/push/subscription", {
@@ -126,11 +126,13 @@ export default function NotificacaoPermissao() {
 
 function urlBase64ToUint8Array(base64String: string): Uint8Array {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
-  const rawData = window.atob(base64);
+  const base64 = (base64String + padding)
+    .replace(/-/g, "+")
+    .replace(/_/g, "/");
+  const rawData = atob(base64);
   const outputArray = new Uint8Array(rawData.length);
   for (let i = 0; i < rawData.length; ++i) {
     outputArray[i] = rawData.charCodeAt(i);
   }
-  return outputArray.buffer as unknown as Uint8Array;
+  return outputArray;
 }
